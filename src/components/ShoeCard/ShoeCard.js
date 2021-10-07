3aa4e5b282a2185097640939eba52fad34d6613d
XylophoneHero <nick97000@gmail.com>
2021-10-07 23:08:46 +0100

    Added sale card

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -37,13 +37,21 @@ const ShoeCard = ({
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
         </ImageWrapper>
+        {variant !== "default" && (
+          <VariantTag variant={variant}>
+            {variant === "on-sale" ? "Sale" : "Just Released!"}
+          </VariantTag>
+        )}
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price onSale={!!salePrice}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" && (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          )}
         </Row>
       </Wrapper>
     </Link>
@@ -56,10 +64,14 @@ const Link = styled.a`
   flex: 1 1 350px;
 `
 
-const Wrapper = styled.article``
+const Wrapper = styled.article`
+  position: relative;
+`
 
 const ImageWrapper = styled.div`
   position: relative;
+  border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
 `
 
 const Image = styled.img`
@@ -77,10 +89,26 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `
 
-const Price = styled.span``
+const Price = styled.span`
+  text-decoration: ${(p) => p.onSale && "line-through"};
+  color: ${(p) => p.onSale && COLORS.gray[700]};
+`
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
+`
+
+const VariantTag = styled.span`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  padding: 8px 12px;
+  border-radius: 2px;
+  color: ${COLORS.white};
+  font-weight: 600;
+  font-size: 0.875rem;
+  background-color: ${(p) =>
+    p.variant === "on-sale" ? COLORS.primary : COLORS.secondary};
 `
 
 const SalePrice = styled.span`
